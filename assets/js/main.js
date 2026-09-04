5ccc7a0a8939986c0336e8e6adb40777099fe9fd
bsscvb-maker <bsscvb@boneshakerssocialclub.com>
2026-09-04 08:33:15 -0400

    Keep page content clear of fixed header

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,22 @@
 (function(){
+  function syncHeaderOffset(){
+    var header=document.querySelector('.globalHeader');
+    if(header){
+      document.body.classList.add('bssc-fixed-header');
+      document.documentElement.style.setProperty('--bssc-header-h',header.offsetHeight+'px');
+    }
+  }
+  syncHeaderOffset();
+  window.addEventListener('load',syncHeaderOffset);
+  window.addEventListener('resize',syncHeaderOffset);
+
   var mobileBtn=document.getElementById('mobileNavButton');
   var mainNav=document.getElementById('mainNav');
   if(mobileBtn && mainNav){
     mobileBtn.addEventListener('click',function(){
       var open=mainNav.classList.toggle('mobileOpen');
       mobileBtn.setAttribute('aria-expanded',open ? 'true':'false');
+      setTimeout(syncHeaderOffset,0);
     });
   }
   document.querySelectorAll('.navButton').forEach(function(btn){
